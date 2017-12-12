b946a35c0c86dca3d1a704501f561b85ca0893fc
a021672 <cloudxpc@126.com>
2017-12-12 12:09:29 +0800

    Initial config

---
+++ src/main/java/com/uutic/website/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.uutic.website.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
